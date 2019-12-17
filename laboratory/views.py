0043f75da185bd001ecdc79db447b33b8e108ddd
anscor <xlyanscor@outlook.com>
2019-12-17 09:04:25 +0800

    预约重复检查bug修复

--- laboratory/views.py
+++ laboratory/views.py
@@ -27,3 +27,27 @@ class LaboratoryTypeViewSet(viewsets.ModelViewSet):
     @wrap_permission(permissions.IsAdminUser)
     def destroy(self, request, *args, **kwargs):
         return super().destroy(request, *args, **kwargs)
+
+
+class LaboratoryViewSet(viewsets.ModelViewSet):
+    queryset = Laboratory.objects.all()
+    serializer_class = LaboratorySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        return Response(data="", status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def update(self, request, *args, **kwargs):
+        return Response(data="", status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def partial_update(self, request, *args, **kwargs):
+        return Response(data="", status=status.HTTP_405_METHOD_NOT_ALLOWED)
--- reservation/views.py
+++ reservation/views.py
@@ -80,13 +80,17 @@ class ReservationTimeViewSet(viewsets.ModelViewSet):
 
     @wrap_permission(permissions.AllowAny)
     def retrieve(self, request, *args, **kwargs):
-        time = ReservationTime.objects.all().filter(id=self.kwargs.get("pk", 0))
+        time = ReservationTime.objects.all().filter(
+            id=self.kwargs.get("pk", 0)
+        )
         if not time:
             return return_not_find("预约时间段不存在！")
         time = time[0]
 
         data = ReservationTimeSerializer(time).data
-        reserved_num = Reservation.objects.all().filter(time_id=time.id).count()
+        reserved_num = (
+            Reservation.objects.all().filter(time_id=time.id).count()
+        )
         data["reserved_num"] = reserved_num
         return Response(data=data, status=status.HTTP_200_OK)
 
@@ -203,7 +207,11 @@ class ReservationViewSet(viewsets.ModelViewSet):
         # 验证是不是已经有过预约
         if (
             Reservation.objects.all()
-            .filter(Q(patient_id=data.get("patient", 0)) & Q(time_id=time_id))
+            .filter(
+                Q(patient_id=data.get("patient", 0))
+                & Q(time_id=time_id)
+                & Q(date=date)
+            )
             .exists()
         ):
             return return_param_error("此时间段已经预约，不可重复预约！")
